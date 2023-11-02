70700d94949d44aa16a8f231c33efb10c76ec6ce
ezzz2 <Antoni.piech4@gmail.com>
2023-11-02 07:46:53 +0100

    outil en neogeum

---
+++ src/main/java/ch/piyamon/neogeum/util/ModTags.java
@@ -0,0 +1,35 @@
+package ch.piyamon.neogeum.util;
+
+import ch.piyamon.neogeum.Neogeum;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Items {
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(Neogeum.MOD_ID, name));
+        }
+
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
+        }
+    }
+
+    public static class Blocks {
+        public static final TagKey<Block> NEEDS_NEOGEUM_TOOL = tag("needs_neogeum_tool");
+
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(Neogeum.MOD_ID, name));
+        }
+
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
+        }
+    }
+}
