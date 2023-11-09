00cd94648d23b4a6b986db2f1e40957d84e77445
diego <piyamon@protonmail.com>
2023-11-09 22:21:44 +0100

    change textures

--- src/main/java/ch/piyamon/neogeum/item/ModFoodProperties.java
+++ src/main/java/ch/piyamon/neogeum/item/ModFoodProperties.java
@@ -5,7 +5,6 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoodProperties {
-    public static final FoodProperties NEOGEUMPOTATO = new FoodProperties.Builder().nutrition(3).saturationMod(0.25f)
-            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
+    public static final FoodProperties NEOGEUMPOTATO = (new FoodProperties.Builder()).nutrition(4).saturationMod(1.2F).effect(new MobEffectInstance(MobEffects.REGENERATION, 400, 1), 1.0F).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 6000, 0), 1.0F).effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 6000, 0), 1.0F).effect(new MobEffectInstance(MobEffects.ABSORPTION, 2400, 3), 1.0F).alwaysEat().build();
 
 }
